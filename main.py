30de7257d1d52b9a36e69dcfa585a1a5b989df4a
Lord Basil <126736516+Drlordbasil@users.noreply.github.com>
2023-09-10 21:09:49 -0400

    AI-enhanced and auto-formatted code

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 from requests import get
-Fix:
-```python
-```
+
+if __name__ == "__main__":
+    # Change the URL to your desired endpoint
+    response = get("https://api.example.com/data")
+    if response.status_code == 200:
+        data = response.json()
+        # Process the data as needed
+        print(data)
+    else:
+        print("Error: Failed to get data from the API.")
